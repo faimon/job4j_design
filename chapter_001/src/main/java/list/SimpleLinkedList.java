dcf271f74b50e13844690b572271ddb77c76179c
andreykatanov <faimon96@gmail.com>
2020-08-08 12:46:59 +0700

    2. Создать контейнер на базе связанного списка [#338020]

---
+++ chapter_001/src/main/java/list/SimpleLinkedList.java
@@ -0,0 +1,76 @@
+package list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements Iterable<E> {
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
+
+    public void add(E value) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(value, l, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> current;
+        if (index < (size >> 1)) {
+            current = first;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = last;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+           private int modCount = size;
+           private int position = 0;
+            @Override
+            public boolean hasNext() {
+                if (modCount != size) {
+                    throw new ConcurrentModificationException();
+                }
+                return position < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return get(position++);
+            }
+        };
+    }
+
+    public static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E item, Node<E> prev, Node<E> next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+}
---
+++ chapter_001/src/test/java/list/SimpleLinkedListTest.java
@@ -0,0 +1,60 @@
+package list;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleLinkedListTest {
+    @Test
+    public void whenAddAndGet() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.add(12);
+        list.add(1);
+        list.add(4);
+        assertThat(list.get(0), is(12));
+        assertThat(list.get(1), is(1));
+        assertThat(list.get(2), is(4));
+    }
+
+    @Test
+    public void whenAddOneElem() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.add(12);
+        assertThat(list.get(0), is(12));
+    }
+
+    @Test
+    public void whenAddTwoElem() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.add(12);
+        list.add(100500);
+        assertThat(list.get(0), is(12));
+        assertThat(list.get(1), is(100500));
+    }
+
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenConcurentModificationException() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.add(12);
+        list.add(1);
+        Iterator it = list.iterator();
+        list.add(4);
+        it.next();
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenNoSuchElementException() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.add(12);
+        list.add(1);
+        Iterator it = list.iterator();
+        it.next();
+        it.next();
+        it.next();
+    }
+}
