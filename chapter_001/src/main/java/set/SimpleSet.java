71f8fbf20dd8fe36fb7cc15442ab53c7cb229d4e
andreykatanov <faimon96@gmail.com>
2020-08-09 20:59:56 +0700

    SimpleSet # edit

--- chapter_001/src/main/java/set/SimpleSet.java
+++ chapter_001/src/main/java/set/SimpleSet.java
@@ -3,6 +3,7 @@ package set;
 import generics.SimpleArray;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleSet<E> implements Iterable<E> {
     private SimpleArray<E> array = new SimpleArray<>(10);
@@ -25,7 +26,7 @@ public class SimpleSet<E> implements Iterable<E> {
     private boolean notContainsValue(E value) {
         boolean rsl = true;
         for (E elem: array) {
-            if (elem.equals(value)) {
+            if (Objects.equals(elem, value)) {
                 rsl = false;
                 break;
             }
