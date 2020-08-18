a0e192d58a6aa98fbd7a717a3ee1450249ddf37b
andreykatanov <faimon96@gmail.com>
2020-08-18 13:24:17 +0700

    0.4. BufferedOutputStream [#338069]

--- chapter_002/src/main/java/io/LogFilter.java
+++ chapter_002/src/main/java/io/LogFilter.java
@@ -1,7 +1,6 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,11 +16,22 @@ public class LogFilter {
         return rsl;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String st: log
+                 ) {
+                out.write(st + System.lineSeparator());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        for (String st: log
-             ) {
-            System.out.println(st);
-        }
+        save(log, "404.txt");
     }
 }
--- chapter_002/src/main/java/io/ResultFile.java
+++ chapter_002/src/main/java/io/ResultFile.java
@@ -1,12 +1,16 @@
 package io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("1 2 3 4 \n".getBytes());
-            out.write("5 6 7 8".getBytes());
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
+            out.write("Hello world!");
         } catch (Exception e) {
             e.printStackTrace();
         }
