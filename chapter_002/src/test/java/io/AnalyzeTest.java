918acb07e7850892312cd25ff07dcf1d7485626a
andreykatanov <faimon96@gmail.com>
2020-08-19 21:18:30 +0700

    3.0. Тестирование IO [#338065]

--- chapter_002/src/test/java/io/AnalyzeTest.java
+++ chapter_002/src/test/java/io/AnalyzeTest.java
@@ -1,22 +1,55 @@
 package io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 public class AnalyzeTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void whenHaveTwo400() throws IOException {
+    public void whenReadLog1() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.print("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
+        }
         Analyze analyze = new Analyze();
-        analyze.unavailable("src/main/data/server.log", "src/main/data/check.log");
-        BufferedReader reader = new BufferedReader(new FileReader("src/main/data/check.log"));
-        assertThat(reader.readLine(), is("500 10:57:01;200 10:59:01"));
-        assertThat(reader.readLine(), is("500 11:01:02;200 11:02:02"));
-        assertThat(reader.readLine(), is(nullValue()));
+        analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            assertThat(in.readLine(), is("500 10:57:01;200 10:59:01"));
+            assertThat(in.readLine(), is("500 11:01:02;200 11:02:02"));
+        }
+    }
+
+    @Test
+    public void whenReadLog2() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.print("400 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
+        }
+        Analyze analyze = new Analyze();
+        analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            assertThat(in.readLine(), is("400 10:56:01;200 10:59:01"));
+            assertThat(in.readLine(), is("500 11:01:02;200 11:02:02"));
+        }
     }
 }
