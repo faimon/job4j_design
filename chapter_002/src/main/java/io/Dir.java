fdb3714afde2a9cbc07906599a181638a46be0d2
andreykatanov <faimon96@gmail.com>
2020-08-19 21:28:52 +0700

    4.0. File [#338070]

---
+++ chapter_002/src/main/java/io/Dir.java
@@ -0,0 +1,18 @@
+package io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("/Users/andreykatanov/IdeaProjects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getName() + " " + subfile.getUsableSpace());
+        }
+    }
+}
